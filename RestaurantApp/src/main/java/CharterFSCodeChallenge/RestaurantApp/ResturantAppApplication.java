026fc9c8229f1f1fd9892cc0fc472a9df9b7a2a5
sampeaslee <speaslee@wisc.edu>
2020-10-28 22:06:00 -0600

    initial project set up

---
+++ RestaurantApp/src/main/java/CharterFSCodeChallenge/RestaurantApp/ResturantAppApplication.java
@@ -0,0 +1,13 @@
+package CharterFSCodeChallenge.RestaurantApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResturantAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResturantAppApplication.class, args);
+	}
+
+}
---
+++ RestaurantApp/src/test/java/CharterFSCodeChallenge/ResturantApp/ResturantAppApplicationTests.java
@@ -0,0 +1,13 @@
+package CharterFSCodeChallenge.ResturantApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResturantAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
